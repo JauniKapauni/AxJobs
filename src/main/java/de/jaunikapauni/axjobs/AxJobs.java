3fbdc3b20b77c00f36e372a89bb966183dc0f320
JauniKapauni <riches-goods.54@icloud.com>
2026-07-04 11:10:14 +0200

    feat: add DatabaseManager

--- src/main/java/de/jaunikapauni/axjobs/AxJobs.java
+++ src/main/java/de/jaunikapauni/axjobs/AxJobs.java
@@ -1,16 +1,31 @@
 package de.jaunikapauni.axjobs;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import de.jaunikapauni.axjobs.manager.DatabaseManager;import org.bukkit.Bukkit;import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxJobs extends JavaPlugin {
+    DatabaseManager databaseManager;
+    public DatabaseManager getDatabaseManager(){
+        return databaseManager;
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        saveDefaultConfig();
+        try{
+            databaseManager = new DatabaseManager(this);
+            if(databaseManager.initDatabaseTable1() == false){
+                getLogger().severe("Error creating table");
+                Bukkit.getServer().shutdown();
+            }
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        databaseManager.close();
     }
 }
